3497b4b9aa3919c6328463927bc8790605e680e2
Gosha <polyakov.geo3@gmail.com>
2023-10-15 17:41:22 +0300

    Решение задачи Секунды

---
+++ Секунды/Секунды.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(0, "RU");
+	int x;
+	printf("Введите значение (в сек): ");
+	scanf_s("%d", &x);
+	printf("%d:", (x / 3600));
+	printf("%d:", (x / 60) % 60);
+	printf("%d", (x % 60));
+	return 0;
+}
