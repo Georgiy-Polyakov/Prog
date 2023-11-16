f860da13af2cbf318df51b05458d72900cab4d46
Gosha <polyakov.geo3@gmail.com>
2023-11-16 21:06:51 +0300

    Решение задач Грибы, Существование треугольника

---
+++ Грибы/Грибы.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int x;
+	printf("введ кол-во грибов: ");
+	scanf_s("%d", &x);
+	if (x % 100 >= 11 && x % 100 <= 14)
+	{
+		printf("грибов");
+	}
+	else
+	{
+		if (x % 10 == 1)
+		{
+			printf("гриб");
+		}
+		else if (x % 10 >= 2 && x % 10 <= 4)
+		{
+			printf("гриба");
+		}
+		else
+		{
+			printf("грибов");
+		}
+	}
+	return 0;
+}
---
+++ Существование треугольника/Существование треугольника.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a, b, c;
+    printf("введите длинну отрезков: ");
+    scanf_s("%d %d %d", &a, &b, &c);
+    if (c < a + b && a < c + b && b < a + c)
+    {
+        if (pow(a, 2) == pow(b, 2) + pow(c, 2) || pow(b, 2) == pow(c, 2) + pow(a, 2) || pow(c, 2) == pow(a, 2) + pow(b, 2))
+        {
+            printf("треугольник прямоугольный");
+        }
+        if (pow(a, 2) < pow(b, 2) + pow(c, 2) || pow(b, 2) < pow(c, 2) + pow(a, 2) || pow(c, 2) < pow(a, 2) + pow(b, 2))
+        {
+            printf("треугольник остроугоольный");
+        }
+        else
+        {
+            printf("треугольник тупоугольный");
+        }
+    }
+    else
+    {
+        printf("Не сущ.");
+    }
+    return 0;
+}
