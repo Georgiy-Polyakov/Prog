cfaa0df9a60ca4013a397cb65a2d638078533de6
Gosha <polyakov.geo3@gmail.com>
2023-11-18 12:04:00 +0300

    Решение задач Таблица умножения и Максимальное число

---
+++ Максимальное_число/Максимальное_число.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(0, "");
+    int n;
+    printf("введ кол-во чисел последовательности: ");
+    scanf_s("%d", &n);
+    int x;
+    int maxNum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf_s("%d", &x);
+        if(x > maxNum)
+        {
+            maxNum = x;
+        }
+    }
+    printf("%d", maxNum);
+}
---
+++ Таблица_умножения/Таблица_умножения.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(0, "");
+	int n;
+	printf("введ. значение:");
+	scanf_s("%d", &n);
+	int z;
+	for (int i = 1; i <= 10; i++)
+	{
+		z = i * n;
+		printf("\n%d*%d=%d", i, n, z);
+	}
+	
+	return 0;
+}
